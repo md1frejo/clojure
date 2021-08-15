8a3e68a8de4751edbda5ee66ec1c290750be07e1
Matthew Molloy <whamtet@gmail.com>
2021-08-16 06:45:07 +0900

    fixed bug

--- test/src/clj/test/routes/home.clj
+++ test/src/clj/test/routes/home.clj
@@ -1,23 +1,7 @@
 (ns test.routes.home
   (:require
     [ctmx.core :as ctmx]
-    [ctmx.render :as render]
-    [hiccup.page :refer [html5]]))
-
-(defn html-response [body]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body body})
-
-(defn html5-response
-  ([body]
-   (html-response
-    (html5
-     [:head
-      [:meta {:name "viewport"
-              :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]]
-     [:body (render/walk-attrs body)]
-     [:script {:src "https://unpkg.com/htmx.org@1.5.0"}]))))
+    [test.response :as response]))
 
 (ctmx/defcomponent ^:endpoint root [req ^:int num-clicks]
   [:div.m-3 {:hx-post "root"
@@ -29,6 +13,6 @@
   (ctmx/make-routes
    "/"
    (fn [req]
-     (html5-response
+     (response/html5-response
       (root req 0)))))
 
--- test/src/clj/test/routes/name.clj
+++ test/src/clj/test/routes/name.clj
@@ -1,13 +1,16 @@
 (ns test.routes.name
   (:require
-    [ctmx.core :as ctmx]))
+    [ctmx.core :as ctmx]
+    [test.response :as response]))
 
 (ctmx/defcomponent ^:endpoint hello [req my-name]
   [:div#hello "hello " my-name])
 
 (defn routes []
-  (ctmx/make-routes "/name" (fn [req] 
-                            [:div 
-                             [:label "what is your name?"]
-                             [:input {:name "my-name" :hx-patch "hello" :hx-target "#hello"}]
-                             (hello req "")])))
+  (ctmx/make-routes "/name"
+                    (fn [req]
+                      (response/html5-response
+                       [:div
+                        [:label "what is your name?"]
+                        [:input {:name "my-name" :hx-patch "hello" :hx-target "#hello"}]
+                        (hello req "")]))))
